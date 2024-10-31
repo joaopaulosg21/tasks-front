7d5dc59578ca0d66bf24c948370dd34b428ef871
joaopaulo <joaopaulosg17@gmail.com>
2024-10-31 18:22:33 -0300

    feat: Mudança no metodo para salvar posição alterada do elemento na lista

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -12,7 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { TaskService } from '../../services/task.service';
 import { MatDialog } from '@angular/material/dialog';
 import { EditTaskDialogContentComponent } from '../edit-task-dialog-content/edit-task-dialog-content.component';
-import {DatePipe} from '@angular/common'
+import { DatePipe } from '@angular/common'
 import { NewTaskDialogContentComponent } from '../new-task-dialog-content/new-task-dialog-content.component';
 import { DeleteTaskDialogContentComponent } from '../delete-task-dialog-content/delete-task-dialog-content.component';
 
@@ -20,11 +20,12 @@ import { DeleteTaskDialogContentComponent } from '../delete-task-dialog-content/
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CdkDrag, CdkDragPlaceholder, CdkDropList, MatListModule, MatIconModule, MatButtonModule,DatePipe],
+  imports: [CdkDrag, CdkDragPlaceholder, CdkDropList, MatListModule, MatIconModule, MatButtonModule, DatePipe],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent implements OnInit {
+  tasks!: any[];
   constructor(private taskService: TaskService, private dialog: MatDialog) { }
   ngOnInit(): void {
     this.findAll();
@@ -32,22 +33,38 @@ export class MainComponent implements OnInit {
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+    //Pega o id do item que está no array de tasks
+    const id = event.item.element.nativeElement.textContent?.split(" ")[1];
+
+    //Altera o atributo presentationOrder para atualizar a posição de exibição do elemento 
+    this.tasks.filter(i => i.id == id)[0].presentationOrder = event.currentIndex;
+    
+    this.taskService.updateOrder(Number(id), event.currentIndex).subscribe({
+      next: (data: any) => {
+        console.log("Atualizado");
+        this.ngOnInit();
+      },
+      error: err => {
+        console.log("DEU ERRO " + err.message);
+      }
+    });
   }
 
-  tasks: any;
   findAll() {
     this.taskService.findAll().subscribe((data: any) => {
       this.tasks = data.content;
-    })
+
+      console.log(this.tasks)
+    });
   }
 
-  openEditDialog(taskId: number, taskName:string, taskCost:string, taskDeadline:string) {
+  openEditDialog(taskId: number, taskName: string, taskCost: string, taskDeadline: string) {
     this.dialog.open(EditTaskDialogContentComponent, {
       data: {
         taskId: taskId,
-        taskName:taskName,
-        taskCost:taskCost,
-        taskDeadline:taskDeadline
+        taskName: taskName,
+        taskCost: taskCost,
+        taskDeadline: taskDeadline
       }
     });
   }
@@ -56,11 +73,11 @@ export class MainComponent implements OnInit {
     this.dialog.open(NewTaskDialogContentComponent);
   }
 
-  deleteDialog(taskId:number, taskName:string) {
-    this.dialog.open(DeleteTaskDialogContentComponent,{
-      data:{
-        taskId:taskId,
-        taskName:taskName
+  deleteDialog(taskId: number, taskName: string) {
+    this.dialog.open(DeleteTaskDialogContentComponent, {
+      data: {
+        taskId: taskId,
+        taskName: taskName
       }
     });
   }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -14,15 +14,19 @@ export class TaskService {
     return this.http.get("http://localhost:8080/task/");
   }
 
-  editTask(data: TaskRegisterData, taskId:number) {
-    return this.http.patch("http://localhost:8080/task/"+taskId,data)
+  editTask(data: TaskRegisterData, taskId: number) {
+    return this.http.patch("http://localhost:8080/task/" + taskId, data)
   }
 
-  addTask(data:TaskRegisterData) {
-    return this.http.post("http://localhost:8080/task/",data);
+  addTask(data: TaskRegisterData) {
+    return this.http.post("http://localhost:8080/task/", data);
   }
 
-  deleteTask(taskId:number) {
-    return this.http.delete("http://localhost:8080/task/"+taskId);
+  deleteTask(taskId: number) {
+    return this.http.delete("http://localhost:8080/task/" + taskId);
+  }
+
+  updateOrder(taskId: number, presentationOrder: number) {
+    return this.http.patch("http://localhost:8080/task/order/" + taskId, { presentationOrder: presentationOrder });
   }
 }
